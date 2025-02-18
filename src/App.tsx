ef25a580cdc2cf460d3546b27c5c7736b472ae67
Moiseev Ilya <ilya@mois.pro>
2025-02-18 11:02:26 +0400

    feat: enhance wallet integration with SendTransaction feature and extra currency support

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
 import './App.scss';
 import './trackers';
-import {THEME, TonConnectUIProvider} from "@tonconnect/ui-react";
-import {Footer} from "./components/Footer/Footer";
-import {Header} from "./components/Header/Header";
-import {TxForm} from "./components/TxForm/TxForm";
+import { RequireFeature, THEME, TonConnectUIProvider, SendTransactionFeature } from '@tonconnect/ui-react';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { TxForm } from './components/TxForm/TxForm';
+
+const walletsRequiredFeatures: RequireFeature[] = [
+    {
+        name: 'SendTransaction',
+        extraCurrencyRequired: true,
+    },
+];
+
+// const walletsRequiredFeatures = (features: ModernFeature[]) => {
+//     return features.find(feature => feature.name === 'SendTransaction' && "requiresExtraCurrency" in feature && feature.requiresExtraCurrency) !== undefined;
+// }
 
 function App() {
-  return (
-    <TonConnectUIProvider
-      manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
-      uiPreferences={{theme: THEME.DARK}}
-      requiredFeatures={[{
-        name: 'SendTransaction',
-        maxMessages: 1,
-        supportsExtraCurrencies: true,
-      }]}
-      // primaryWalletAppName={'tonkeeper'}
-      actionsConfiguration={{
-        twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
-      }}
-    >
-      <div className="app">
-        <Header/>
-        <TxForm/>
-        {/*<TonProofDemo />*/}
-        <Footer/>
-      </div>
-    </TonConnectUIProvider>
-  )
+    return (
+        <TonConnectUIProvider
+            manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+            uiPreferences={{ theme: THEME.DARK }}
+            walletsRequiredFeatures={walletsRequiredFeatures}
+            walletsListConfiguration={{
+              includeWallets: [
+                {
+                  appName: "tonkeeper_ec",
+                  name: "Tonkeeper EC",
+                  imageUrl: "https://tonkeeper.com/assets/tonconnect-icon.png",
+                  tondns: "tonkeeper.ton",
+                  aboutUrl: "https://tonkeeper.com",
+                  deepLink: "tonkeeper-tc://",
+                  universalLink: 'https://app.tonkeeper.com/ton-connect',
+                  features: [
+                      {
+                          name: "SendTransaction",
+                          maxMessages: 255,
+                          extraCurrencySupported: true
+                      }
+                  ],
+                  jsBridgeKey: 'tonkeeper',
+                  platforms: ["chrome", "firefox"]
+              },
+              ]
+            }}
+            // primaryWalletAppName={'tonkeeper'}
+            actionsConfiguration={{
+                twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start',
+            }}
+        >
+            <div className="app">
+                <Header />
+                <TxForm />
+                {/*<TonProofDemo />*/}
+                <Footer />
+            </div>
+        </TonConnectUIProvider>
+    );
 }
 
-export default App
+export default App;
